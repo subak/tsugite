1e2c5460858dbe00ea790d28a113dd68b5f73ba9
Subak Systems <info@subak.jp>
2019-05-23 17:16:17 +0900

    構造を修正

---
+++ ui/select/index.js
@@ -0,0 +1,48 @@
+const {propEq,pipe,juxt,flatten,apply,remove} = require('ramda');
+const {merge} = require('rxjs');
+const {tap,filter,map,withLatestFrom} = require('rxjs/operators');
+const {paths} = require('../../../../vendor/subak/ramda')
+
+const create = (ns, state, view, options) => {
+  const NS = ns.join('.')
+
+  const hooks = ({events$}) => ({
+    vnode$: events$.pipe(
+      filter(propEq(0, `${NS}.vnode`)),
+      map(remove(0,1))),
+    change$: events$.pipe(
+      filter(propEq(0, `${NS}.change`)),
+      map(remove(0,1)))
+  })
+
+  const vnode = ([events$, value$, options$]) =>
+    value$.pipe(
+      tap(value =>
+        value),
+      withLatestFrom(options$),
+      map(([[value, ...args], options]) =>
+        [
+          view({
+            value,
+            options,
+            change: ev =>
+              events$.next([`${NS}.change`, ev, ...args])}),
+          ...args
+        ]),
+      map(([vnode, ...args]) =>
+        [`${NS}.vnode`, vnode, ...args]))
+
+  const change = ([change$, update]) =>
+    change$.pipe(
+      map(([{target:{value}}, ...args]) =>
+        update(value, ...args)))
+
+  const events = pipe(juxt([
+    pipe(paths([['events$'], [...state, 'value$'], options]), vnode),
+    pipe(paths([[...ns, 'change$'], [...state, 'update']]),change)
+  ]), flatten, apply(merge))
+
+  return {hooks, events}
+}
+
+module.exports = {create}
